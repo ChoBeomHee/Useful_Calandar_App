a108ca34a0a7ac0e1ccfe1d01786e1a339b3723f
nayeone <ice1011@naver.com>
2022-11-17 13:53:51 +0900

    이나연연연

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ class _HomeState extends State<Home> {         // 메인 페이지
                     border: Border.all(color: Colors.black12, width: 3)), //테두리
                 width: 550,
                 height: 150,
-                child: Text('\n    이나연깃..일까?', style: TextStyle(fontSize: 30),),
+                child: Text('\n    이나연깃233..일까?', style: TextStyle(fontSize: 30),),
               ),
             ),
           ],
