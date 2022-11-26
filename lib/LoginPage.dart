0362ab19e0d30bfa9c513e6e282d950bdfb89579
BickBird <waujoxc@gmail.com>
2022-11-26 14:52:30 +0900

    j

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:team/CalendarPage.dart';
 import 'RegisterPage.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -78,8 +79,8 @@ class _LoginFormState extends State<LoginForm> {
                       if (currentUser.user != null) {
                         _formkey.currentState!.reset();
                         if (!mounted) return;
-                        //Navigator.push(context,
-                        //MaterialPageRoute(builder: (context) => ChatPage()));
+                        Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => CalendarPage()));
                       }
                     } catch (e) {
                       print(e);
